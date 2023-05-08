c417971bc45d810157d1cc3a1550e939b5e9def4
Gwenyfar <iruoma.onyia@cyphercrescent.com>
2023-05-08 17:10:31 +0100

    Add project files.

---
+++ Authentication/Models/LogIn.cs
@@ -0,0 +1,8 @@
+﻿namespace Authentication.Models
+{
+    public class LogIn
+    {
+        public string UserName { get; set; } 
+        public string Password { get; set; } 
+    }
+}
---
+++ Authentication/Services/NHibernate.cs
@@ -0,0 +1,16 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+
+namespace Authentication.Services
+{
+    public class NHibernate
+    {
+        public ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2000.ConnectionString(@""))
+        }
+
+    }
+}

6ab3f1391335d75f21c1f10b50b057d383e0b109
Gwenyfar <iruoma.onyia@cyphercrescent.com>
2023-05-09 17:52:37 +0100

    First draft of controller

---
+++ Authentication/Controllers/AuthenticaionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authentication.Controllers
+{
+    [Authorize]
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticaionController : ControllerBase
+    {
+        public AuthenticaionController()
+        {
+                
+        }
+        [HttpPost]
+        public IActionResult SignIn(SignInDto signIn)
+        {
+
+        }
+    }
+}
---
+++ Authentication/Controllers/SignInDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Authentication.Controllers
+{
+    public class SignInDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}

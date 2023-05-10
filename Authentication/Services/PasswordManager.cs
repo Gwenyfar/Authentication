b733b2dea5c827cd87bffb1190285d9dcd27076b
Gwenyfar <iruoma.onyia@cyphercrescent.com>
2023-05-10 06:27:11 +0100

    password hashing added

---
+++ Authentication/Services/PasswordManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+namespace Authentication.Services
+{
+    public class PasswordManager
+    {
+        public static string HashPassword(string password)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            var pbkdf2Service = new Rfc2898DeriveBytes(password, salt, 100);
+            var hash = pbkdf2Service.GetBytes(20);
+            var hashBytes = new byte[36];
+
+            Array.Copy(salt,0,hashBytes,0,16);
+            Array.Copy(hash,0,hashBytes,16,20);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+}
--- Authentication/Services/Repository.cs
+++ Authentication/Services/Repository.cs
@@ -2,18 +2,30 @@
 
 namespace Authentication.Services
 {
-    public class Repository
+    public class Repository : IRepository
     {
         public Repository()
         {
             Nhibernate = new Nhibernate();
         }
         public Nhibernate Nhibernate { get; set; }
-        
+
+        public void AddUser(User user)
+        {
+            Nhibernate.Session.Save(user);
+        }
+
+        public bool AuthenticateUser(string password, string passwordHash)
+        {
+            return (PasswordManager.HashPassword(password) == passwordHash);
+        }
+
         public User GetUser(string userName)
         {
             var user = Nhibernate.Session.Query<User>().FirstOrDefault(u => u.UserName.ToLower().Equals(userName.ToLower()));
             return user;
         }
+
+        
     }
 }

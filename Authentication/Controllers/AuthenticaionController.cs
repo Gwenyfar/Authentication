11ad1b9b21619e11c2b3f789d935108c767ea138
Gwenyfar <iruoma.onyia@cyphercrescent.com>
2023-06-27 09:14:31 +0100

    quick fixes

--- Authentication/Controllers/AuthenticaionController.cs
+++ Authentication/Controllers/AuthenticaionController.cs
@@ -66,7 +66,7 @@ namespace Authentication.Controllers
             return Ok("Sign in successful");
         }
 
-        [HttpPost]
+        [HttpPost("logout")]
         public IActionResult SignOut()
         {
             HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
--- Authentication/Controllers/SignUpDto.cs
+++ Authentication/Controllers/SignUpDto.cs
@@ -1,7 +1,9 @@
 ﻿namespace Authentication.Controllers
 {
-    public class SignUpDto : SignInDto
+    public class SignUpDto
     {
+        public string UserName { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
     }
 }
